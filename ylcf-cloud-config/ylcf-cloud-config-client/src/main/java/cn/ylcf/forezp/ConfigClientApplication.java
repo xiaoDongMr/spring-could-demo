2be676bb5fdf21213693ec1a55a9f9ad272bf031
liuxiaodong <1027331569@qq.com>
2018-08-21 11:47:00 +0800

    updata

---
+++ ylcf-cloud-config/ylcf-cloud-config-client/src/main/java/cn/ylcf/forezp/ConfigClientApplication.java
@@ -0,0 +1,23 @@
+package cn.ylcf.forezp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${server}")
+    String server;
+    @RequestMapping(value = "/server")
+    public String hi(){
+        return server;
+    }
+}
