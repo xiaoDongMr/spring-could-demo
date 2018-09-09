0178b55d1477ef52c43d453bd5e7d6ed44e8756c
xiaoDongMr <1027331569@qq.com>
2018-09-10 01:00:16 +0800

    swagger

--- ylcf-org-cloud-manager/ylcf-org-cloud-service-portal/src/main/java/cn/ylcf/YlcfOrgServicePortalApplication.java
+++ ylcf-org-cloud-manager/ylcf-org-cloud-service-portal/src/main/java/cn/ylcf/YlcfOrgServicePortalApplication.java
@@ -3,9 +3,12 @@ package cn.ylcf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
